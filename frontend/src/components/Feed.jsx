d33a47405838e785a0c38b2dec17bf3322275f6c
marmelad444 <1111marmelad1111@gmail.com>
2025-11-15 07:02:33 +0000

    212

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -1,10 +1,30 @@
+import { useEffect, useState } from "react"
+import { api } from "../api/api"
+import MessageCard from "./MessageCard"
+
 const Feed =({title = "Сообщения"}) =>{
+    const [message, setMessage]= useState([])
+
+    useEffect (() => {
+        const fetchData = async () => {
+            try {
+                const data = await api.getMessages()
+                setMessage(data)
+            } catch (error){
+                console.error(error)
+            }
+        }
+        fetchData()
+    }, [])
     return(
         <>
             <div className="messages-sections">
                 <div className="containter">
                     <h2 className="section-title">{title}</h2>
-                    <div className="message-grig"></div>
+                    <div className="messages-grid">
+                        {message.map((el)=>(
+                        <MessageCard {...el} /> ))}
+                    </div>
                 </div>
             </div>
         </>
